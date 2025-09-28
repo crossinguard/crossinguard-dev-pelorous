43cac41926abaa99b7f744cb45a86f79dc305210
Brett Egbert <brett@crossinguard.dev>
2025-09-28 12:20:56 -0700

    set up posts and projects content collections

---
+++ src/content.config.ts
@@ -0,0 +1,50 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from "astro:content";
+
+// 2. Import loader(s)
+import { glob, file } from "astro/loaders";
+
+// 3. Define your collection(s)
+const posts = defineCollection({
+  loader: glob({
+    pattern: ["**/*.md", "!**/_*.md"],
+    base: "./src/content/posts",
+  }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z
+      .object({
+        src: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    series: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+const projects = defineCollection({
+  loader: glob({
+    pattern: ["**/*.md", "!**/_*.md"],
+    base: "./src/content/projects",
+  }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z
+      .object({
+        src: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    series: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { posts, projects };
---
+++ src/content/posts/post-1.md
@@ -0,0 +1,10 @@
+---
+title: "First Post"
+description: "This is a description"
+pubDate: "2025-09-28"
+---
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quis
+eveniet maxime deserunt tenetur provident repellendus voluptas aliquid
+autem earum alias doloribus, consequuntur dolor! Ratione quaerat ea
+ipsam minus ad!
